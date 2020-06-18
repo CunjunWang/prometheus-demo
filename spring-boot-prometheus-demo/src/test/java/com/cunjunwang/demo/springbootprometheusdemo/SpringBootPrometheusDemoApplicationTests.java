00f379df048c02c875542890df7423093e2d9b72
Cunjun Wang <duckwcj@gmail.com>
2020-06-17 23:37:54 -0400

    prometheus demo for spring

---
+++ spring-boot-prometheus-demo/src/test/java/com/cunjunwang/demo/springbootprometheusdemo/SpringBootPrometheusDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cunjunwang.demo.springbootprometheusdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrometheusDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
